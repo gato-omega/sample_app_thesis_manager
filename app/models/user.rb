dca63d8957026fa29ff1ea1d18236c0236a86a27
codescrum <codescrum@codescrum-mobile-0.(none)>
2014-09-23 18:55:27 -0500

    fix session serialization issue (mongoid and devise) https://github.com/plataformatec/devise/issues/2949

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,9 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+  def self.serialize_into_session(record)
+    [record.id.to_s, record.authenticatable_salt]
+  end
+  
+
 end
