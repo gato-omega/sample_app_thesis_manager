112a4f1f14ce251f320f8b84a0daffa7d64f6c5a
codescrum <codescrum@codescrum-mobile-0.(none)>
2014-09-24 16:03:09 -0500

    add user email field into full text search

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project
   field :name, type: String
   field :description, type: String
 
-  search_in :name, :description
+  search_in :name, :description, user: :email
 
   belongs_to :user
 
